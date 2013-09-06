df9926938a4dc704f56e96fc2b77728c7ae7699a
Hage Yaapa <captain@hacksparrow.com>
2013-09-06 11:44:14 +0530

    auto launch demos

--- demos/index.js
+++ demos/index.js
@@ -1,6 +1,9 @@
 var express = require('express')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , os = require('os')
+  , exec = require('child_process').exec
+  , child;
 
 var app = express();
 
@@ -12,6 +15,19 @@ app.get('/', function(req, res) {
   res.redirect('/demos.html');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Load http://localhost:' + app.get('port') + '/demos.html');
+http.createServer(app).listen(app.get('port'), function() {
+
+  var demo_url = 'http://localhost:' + app.get('port') + '/demos.html';
+  var command;
+
+  if (os.platform() == 'darwin') { command = 'open ' + demo_url; }
+  else if (os.platform() == 'linux') { command = 'xdg-open ' + demo_url; }
+  else { command = 'start ' + demo_url; }
+
+  child = exec(command, function(error, stdout, stderr) {
+    if (error) { console.log(error); }
+  });
+
+  console.log('Running demos at: ' + demo_url);
+
 });
